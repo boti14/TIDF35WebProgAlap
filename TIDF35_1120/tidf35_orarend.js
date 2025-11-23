79b401b78aadb3bbf162bf82a861457ac4393048
SzBotond <szendrei.botond890@gmail.com>
2025-11-23 02:41:10 +0100

    18. Pract

---
+++ TIDF35_1120/tidf35_orarend.js
@@ -0,0 +1,28 @@
+{
+    "TIDF35_orarend":{
+        "ora": [
+            {
+                "targy": "Szoftvertesztelés"
+                "idopont": {
+                    "nap": "hétfő",
+                    "tol": 10,
+                    "ig": 12
+                },
+                "hekyszin": "Inf. 103",
+                "oktato": "Dr. Hornyák Olivér",
+                "szak": "G3BIM"
+            },
+            {
+                "targy": "Szoftvertesztelés"
+                "idopont": {
+                    "nap": "hétfő",
+                    "tol": 10,
+                    "ig": 12
+                },
+                "hekyszin": "Inf. 103",
+                "oktato": "Dr. Hornyák Olivér",
+                "szak": "G3BIM"
+            },
+        ]
+    }
+}
