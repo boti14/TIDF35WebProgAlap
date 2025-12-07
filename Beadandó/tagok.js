130159c5b3e05429074cba430acb9ed1c47ca144
SzBotond <szendrei.botond890@gmail.com>
2025-12-07 02:14:00 +0100

    Beadandó

--- Beadandó/tagok.js
+++ Beadandó/tagok.js
@@ -4,16 +4,19 @@ $(document).ready(function()
     {
         $("#hely").html("");
 
-        $("#hely").append("<h1>Teszt sor kívül</h1>");
+        $("#hely").append("<h1>Szoprán kórustagok</h1>");
 
         $.getJSON("tagok.json", function(data)
         {
 
             $("#hely").append("Teszt sor belül");
 
-            for ( let i = 0; i < data.tagok.length; i ++)
+            for ( let i = 0; i < data.tagok.szopran.length; i ++)
             {
-                $("#hely").append("Név: " + data.tagok[i].nev);
+            
+                $("#hely").append("<b> Név: </b> &nbsp;" + data.szopran[i] + "<br><br>");
+                
+                $("hely").append("<li>" + data.tagok["szopran"][i].nev + ":&nbsp;" + data.tagok["szopran"][i].szolam + "</li>");
             }
 
             if ( data.tagok.szolam = "szoprán")
